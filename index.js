abbb5afac2efa9ad99226a6e07f95e7dff66d841
Mayank Thakur <mayank.sanyank@gmail.com>
2024-07-25 23:00:57 +0530

    added mongodb and sqlite

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const createApplication = require('./lib/mimi');
-const setupSwagger = require('./lib/mimi').setupSwagger;
-const customParser = require('./lib/mimi').customParser;
-const hashPassword = require('./lib/mimi').hashPassword;
-const comparePassword = require('./lib/mimi').comparePassword;
-const generateToken = require('./lib/mimi').generateToken;
-const verifyToken = require('./lib/mimi').verifyToken;
-const authMiddleware = require('./lib/mimi').authMiddleware;
-const Router = require('./lib/mimi').Router;
+// const createApplication = require('./lib');
+// const setupSwagger = require('./lib').setupSwagger;
+// const customParser = require('./lib').customParser;
+// const hashPassword = require('./lib').hashPassword;
+// const comparePassword = require('./lib').comparePassword;
+// const generateToken = require('./lib').generateToken;
+// const verifyToken = require('./lib').verifyToken;
+// const authMiddleware = require('./lib').authMiddleware;
+// const Router = require('./lib').Router;
+// const connectDb = require('./lib').connectDb;
+// const createCollection = require('./lib').createCollection;
 
-module.exports = {
-  mimi: createApplication,
-  setupSwagger,
-  customParser,
-  hashPassword,
-  comparePassword,
-  generateToken,
-  verifyToken,
-  authMiddleware,
-  Router,
-};
+// module.exports = {
+//   mimi: createApplication,
+//   setupSwagger,
+//   customParser,
+//   hashPassword,
+//   comparePassword,
+//   generateToken,
+//   verifyToken,
+//   authMiddleware,
+//   Router,
+//   connectDb,
+//   createCollection,
+// };
+
+module.exports = require('./lib');
